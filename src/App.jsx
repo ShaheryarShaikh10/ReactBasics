9c4bdd4109d5ccc89e6e1249620f2edd01ae6f91
ShaheryarShaikh10 <127825878+ShaheryarShaikh10@users.noreply.github.com>
2025-03-28 16:24:12 +0530

    props,components,usestates

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import {CORE_CONCEPTS} from './data'
+import Header from './components/Header/Header.jsx'
+import CoreConcept from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
+function App() {
+  const [selectedTopic,setSelectedTopic]=useState('Please click on a button');
+  function handleSelect(selectedButton){
+  setSelectedTopic(selectedButton)
+  }
+  return (
+    <div>
+      <Header/>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept  {...CORE_CONCEPTS[0]}/>
+            <CoreConcept  {...CORE_CONCEPTS[1]}/>
+            <CoreConcept  {...CORE_CONCEPTS[2]}/>
+            <CoreConcept  title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={()=>handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={()=>handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={()=>handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={()=>handleSelect("state")}>State</TabButton>
+          </menu>
+          {selectedTopic}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+const reactDescription = ['Fundamental', 'Crucial', 'Core']
+import reactImg from '../../assets/react-core-concepts.png'
+import './Header.css'
+function getRandomInt(max){
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescription[getRandomInt(reactDescription.length - 1)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are 
+        going to build!
+      </p>
+    </header>
+  );
+}
