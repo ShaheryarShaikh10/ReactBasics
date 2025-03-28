4175456f605a59ccc04aeae4c62741825a15651f
ShaheryarShaikh10 <127825878+ShaheryarShaikh10@users.noreply.github.com>
2025-03-28 16:55:15 +0530

    added logic for conditional Example tab rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react';
-import {CORE_CONCEPTS} from './data'
-import Header from './components/Header/Header.jsx'
-import CoreConcept from './components/CoreConcepts.jsx';
-import TabButton from './components/TabButton.jsx';
+import { useState } from "react";
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data";
 function App() {
-  const [selectedTopic,setSelectedTopic]=useState('Please click on a button');
-  function handleSelect(selectedButton){
-  setSelectedTopic(selectedButton)
+  const [selectedTopic, setSelectedTopic] = useState("");
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+  
+  let tabContent = <p>Please select a topic.</p>
+  if(selectedTopic){
+    tabContent = <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code> {EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
   }
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept  {...CORE_CONCEPTS[0]}/>
-            <CoreConcept  {...CORE_CONCEPTS[1]}/>
-            <CoreConcept  {...CORE_CONCEPTS[2]}/>
-            <CoreConcept  title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=>handleSelect("components")}>Components</TabButton>
-            <TabButton onSelect={()=>handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={()=>handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={()=>handleSelect("state")}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {tabContent}
         </section>
       </main>
     </div>
